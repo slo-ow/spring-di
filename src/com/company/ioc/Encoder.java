e2147b28fd35687195c6f78727466ca3537885be
slo_ow <icet25@naver.com>
2022-10-27 16:28:19 +0900

    Explain of IoC & DI and Add class files

--- src/com/company/ioc/Encoder.java
+++ src/com/company/ioc/Encoder.java
@@ -3,7 +3,16 @@ package com.company.ioc;
 import java.util.Base64;
 
 public class Encoder {
-    public String encode(String message){
-        return Base64.getEncoder().encodeToString(message.getBytes());
+
+    private IEncoder iEncoder; // 의존대상 A
+
+    // DI를 주입받음
+    public Encoder(IEncoder iEncoder) {
+        this.iEncoder = iEncoder; // 의존대상 A
+    }
+
+    public String encode(String message)
+    {
+        return iEncoder.encode(message);
     }
 }
--- src/com/company/ioc/Main.java
+++ src/com/company/ioc/Main.java
@@ -1,18 +1,21 @@
 package com.company.ioc;
-
+/*
+* Reference : https://tecoble.techcourse.co.kr/post/2021-04-27-dependency-injection/
+* IoC (Inversion of Control) 란?
+* 제어의 역전이라고 불린다.
+* 의존관계의 제어를 개발자가 직접해 주는것이 아니라, 스프링 프레임워크의 IoC 컨테이너가 제어권을 갖는것.
+* IoC 컨테이너의 역할은 : 객체생성과관리,의존성관리를 대신해 주기 때문에 개발자는 로직에 집중할 수 있는 장점이 있다.
+*
+* 의존관계 주입 DI (Dependency Injection)
+* Dependency (의존관계) 란? : A가 B를 의존한다 -> 의존대상 B가 변하면, 그것이 A에 영향을 미친다.
+* */
 public class Main {
     public static void main(String[] args) {
         String url = "www.naver.com/books/it?page=10&size=20&name=spring-boot";
 
-        // Base 64 encoding
-
-        // url encoding
-
-        Encoder encoder = new Encoder();
+//        Encoder encoder = new Encoder(new Base64Encoder()); // 의존대상 B-1
+        Encoder encoder = new Encoder(new UrlEncoder()); // 의존대상 B-2
         String result = encoder.encode(url);
-
-        UrlEncoder urlEncoder = new UrlEncoder();
-        String urlResult = urlEncoder.encode(url);
-        System.out.println(urlResult);
+        System.out.println(result);
     }
 }
